76207f085f5907b26c1a9125b59af13545646695
mattydolan <matty_dolan@yahoo.com>
2025-01-10 11:03:07 -0500

    key update

--- dockerfile
+++ dockerfile
@@ -1,6 +1,14 @@
 # Use an official Python runtime as the base image
 FROM python:3.12.8
 
+# Set build arguments for AWS credentials (for build-time)
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
+
+# Set environment variables for AWS credentials
+ENV AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+ENV AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+
 # Set the working directory in the container
 WORKDIR /app
 
--- matt.py
+++ matt.py
@@ -9,12 +9,14 @@ m = r.status_code
 mm = r.json()
 print(mm)
 
+/*
 # Create a session using your AWS credentials (or default credentials)
-session = boto3.Session(
-    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    region_name='us-east-2'
-)
+# session = boto3.Session(
+#    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+#    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+#    region_name='us-east-2'
+#)
+
 
 # Create an S3 client
 s3_client = session.client('s3')
